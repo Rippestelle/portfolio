e745eebd96e766de5ce484bf18a30daa94361b58
Estelle Rippe <rippestelle@gmail.com>
2022-03-09 16:33:35 +0100

    modifications about section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="about" id="about">
-    <h2>&lt;	Hey Stranger /&gt;</h2>
+    <h2>&lt;	Hello There /&gt;</h2>
     <h3>Moi c'est <a href="https://www.linkedin.com/in/estelle-rippe/">Estelle</a>, je suis Développeuse Web.</h3>
-    <p>Issue de formation Littéraire, j'ai changé de voie professionnelle et effectué le grand plongeon dans le domaine du Développement Web début 2021. De janvier à juillet 2021,  <strong><a href="https://oclock.io/">l'école O'clock</a></strong> m'a guidé dans les méandres de la programmation web et m'a fourni de solides bases pour faire mes premiers pas en tant que Développeuse Web. J' ai découvert divers langages tels que HTML, CSS, JavaScript, PHP ou encore SQL puis je me suis spécialisée avec le CMS WordPress et le framework Vue.js.</p>
+    <p>Issue de formation Littéraire, j'ai changé de voie professionnelle et effectué le grand plongeon dans le domaine du Développement Web début 2021. De janvier à juillet 2021,  <strong><a href="https://oclock.io/">l'école O'clock</a></strong> m'a guidé dans les méandres de la programmation web et m'a fourni de solides bases pour faire mes premiers pas en tant que Développeuse Web. J'ai découvert divers langages tels que HTML, CSS, JavaScript, PHP ou encore SQL puis je me suis spécialisée avec le CMS WordPress et le framework Vue.js.</p>
     <p>Après la formation, j'ai effectué un stage de deux mois, obtenu mon Titre Professionnel de Développeuse Web et Web Mobile en Septembre 2021 et passé quelques certifications complémentaires afin de monter en compétences et de comprendre tous les enjeux pour la création et la qualité d'un projet web.</p>
-    <p>Aujourd'hui, je recherche un poste afin de gagner de l'expérience entre la Bretagne et la Loire-Atlantique (ou n'importe où en full-remote!) et je propose en attendant mes services pour la création de site internet.</p>
+    <p>Aujourd'hui, je recherche un poste d'alternante dans le cadre d'une nouvelle formation en tant que Concepteur Développeur d'application. Je suis donc à la recherche d'une entreprise entre la Bretagne et la Loire-Atlantique (ou n'importe où en full-remote) afin de gagner de l'expérience et de mettre à profit mes compétences. Je propose en attendant mes services pour la création de site internet.</p>
   </div>
 </template>
 
